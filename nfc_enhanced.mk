c04cc193122f57d9576a936e6a5163a654be29b2
Dan Pasanen <dan.pasanen@gmail.com>
2013-03-12 21:51:09 -0500

    d2: add cm enhanced nfc stuff

---
+++ nfc_enhanced.mk
@@ -0,0 +1,3 @@
+# This device supports CM enhanced NFC
+PRODUCT_COPY_FILES += \
+    device/samsung/d2-common/com.cyanogenmod.nfc.enhanced.xml:system/etc/permissions/com.cyanogenmod.nfc.enhanced.xml

b7907130318b17e169f6ead3275179a4215b7dcc
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-11-07 09:17:36 -0600

    d2: inherit embedded makefile for plasma-mobile
    Change-Id: I6282ba4e130edb94626c1c10d22607528c4523b1

--- d2-common.mk
+++ d2-common.mk
@@ -22,7 +22,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 
 # Inherit from d2-common device
 $(call inherit-product, device/samsung/d2-common/device.mk)
